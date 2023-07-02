f5e3e931912e2f7cbcea5472fb6752c73b7f8a26
magno <magnopacheco67@gmail.com>
2023-07-02 17:03:22 -0300

    wip: trying to solve build error

--- src/components/Actions/DownloadButton/DownloadButton.tsx
+++ src/components/Actions/DownloadButton/DownloadButton.tsx
@@ -1,28 +1,28 @@
-import React from "react";
-import { CloudArrowDown } from "phosphor-react";
+// import React from "react";
+// import { CloudArrowDown } from "phosphor-react";
 
-import { DownloadButtonProps } from "../../../../src/models/Models";
-import { downloadButtonStyle } from "src/utils/default-values";
-import * as S from "./styles";
+// import { DownloadButtonProps } from "../../../../src/models/Models";
+// import { downloadButtonStyle } from "src/utils/default-values";
+// import * as S from "./styles";
 
-export default function DownloadButton({ buttonType }: DownloadButtonProps) {
-  const buttonStyles = downloadButtonStyle[buttonType];
+// export default function DownloadButton({ buttonType }: DownloadButtonProps) {
+//   const buttonStyles = downloadButtonStyle[buttonType];
 
-  return (
-    <S.ButtonContainer
-      $bgColor={buttonStyles["bg_light"]}
-      $textColor={buttonStyles["text_color"]}
-    >
-      <a href="" download={buttonType}>
-        <S.IconWrapper $bgColor={buttonStyles["bg_dark"]}>
-          <CloudArrowDown
-            size={16}
-            color={buttonStyles["text_color"]}
-            weight="bold"
-          />
-        </S.IconWrapper>
-        <div className="button-text">{buttonStyles["label"]}</div>
-      </a>
-    </S.ButtonContainer>
-  );
-}
+//   return (
+//     <S.ButtonContainer
+//       $bgColor={buttonStyles["bg_light"]}
+//       $textColor={buttonStyles["text_color"]}
+//     >
+//       <a href="" download={buttonType}>
+//         <S.IconWrapper $bgColor={buttonStyles["bg_dark"]}>
+//           <CloudArrowDown
+//             size={16}
+//             color={buttonStyles["text_color"]}
+//             weight="bold"
+//           />
+//         </S.IconWrapper>
+//         <div className="button-text">{buttonStyles["label"]}</div>
+//       </a>
+//     </S.ButtonContainer>
+//   );
+// }
